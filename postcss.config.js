7afd136dfbf2ffa894361d9d42a09e54d13ce948
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2026-03-13 21:15:42 +0200

    commite

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
-export default {
+module.exports = {
   plugins: {
-    tailwindcss: {}, // Kthehu te konfigurimi i vjetër
+    tailwindcss: {},
     autoprefixer: {},
-  }
-}
+  },
+};
