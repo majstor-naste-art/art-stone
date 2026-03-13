f5dbf7ccd3f1e785e9235960708ee650237738ab
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2026-03-13 21:35:42 +0200

    commite

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,6 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
 });
